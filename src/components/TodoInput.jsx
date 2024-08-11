266afd660a15ec9531fc49aacfb2cb53a282ad07
krishnak03 <krishnaverma0522@gmail.com>
2024-08-12 00:47:56 +0530

    crud todo app using react js

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,14 @@
+export default function TodoInput(props) {
+    const { handleAddTodos, todoValue, setTodoValue } = props
+    return (
+        <header>
+            <input type="text" placeholder="Enter todo..." value={todoValue} onChange={(e) => {
+                setTodoValue(e.target.value)
+            }} />
+            <button onClick={() => {
+                handleAddTodos(todoValue)
+                setTodoValue('')
+            }}>Add</button>
+        </header>
+    )
+}
